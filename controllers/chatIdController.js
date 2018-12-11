d27d495e7d9eefdc9b9ba1f0b86357be9dbdf32e
Menonro <nitayd2001@gmail.com>
2018-12-11 17:26:07 +0600

    update

--- controllers/chatIdController.js
+++ controllers/chatIdController.js
@@ -1,23 +1,25 @@
 ChatId = require('../models/chatId');
 
 exports.new = function (chatId, status) {
+
+    ChatId.findOne({ chatId: chatId }, function (err, adventure) {
+        if (err) { return console.log("err", err) }
+        console.log(adventure);
+    });
+
     let contact = new ChatId();
     contact.chatId = chatId
     contact.status = status || 1
 // save the contact and check for errors
     contact.save(function (err) {
-        if (err) { console.log('error', err) }
+        if (err) { return console.log('error', err) }
         console.log('New chatId')
     });
 };
 
 exports.get = function (chatId, status) {
     ChatId.get(function (err, chatIds) {
-        if (err) {  console.log('error', err); }
+        if (err) { return console.log('error', err); }
         console.log(chatIds);
-    });
-    ChatId.deleteOne({ chatId: 200221422 }, function (err) {
-      if (err) return handleError(err);
-      // deleted at most one tank document
     });
 };
