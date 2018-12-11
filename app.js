511e4c0f6b1f934fa3773d705c3ec3fef9b89570
Menonro <nitayd2001@gmail.com>
2018-12-11 15:28:40 +0600

    Конфиг

--- app.js
+++ app.js
@@ -12,17 +12,14 @@ const upload = multer(); // for parsing multipart/form-data
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-
 const url = 'mongodb://localhost:27017';
-
 const dbName = 'chatIDs';
-
 const client = new MongoClient(url);
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
   assert.equal(null, err);
-  console.log("Connected successfully to Mongo");
+  console.log(client);
   
   const db = client.db(dbName);
 
