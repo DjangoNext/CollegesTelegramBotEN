22373f523af6baa7666ff613ddb5f86bb4365584
Menonro <nitayd2001@gmail.com>
2018-12-11 17:16:19 +0600

    update

--- controllers/chatIdController.js
+++ controllers/chatIdController.js
@@ -6,14 +6,14 @@ exports.new = function (chatId, status) {
     contact.status = status || 1
 // save the contact and check for errors
     contact.save(function (err) {
-        if (err) { console.log(err) }
+        if (err) { console.log('error', err) }
         console.log('New chatId')
     });
 };
 
 exports.get = function (chatId, status) {
     ChatId.get(function (err, chatIds) {
-        if (err) {  console.log('error': err); }
+        if (err) {  console.log('error', err); }
         console.log(chatIds);
     });
 };
