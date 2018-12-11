c0d9116cd81170db91ae9ed5be0731b0bea1a28f
Menonro <nitayd2001@gmail.com>
2018-12-11 17:32:06 +0600

    update

--- controllers/chatIdController.js
+++ controllers/chatIdController.js
@@ -4,26 +4,24 @@ exports.new = function (chatId, status) {
 
     ChatId.findOne({ chatId: chatId }, function (err, adventure) {
         if (err) { return console.log("err", err) }
-        console.log(adventure);
+        if(!adventure) {
+            let contact = new ChatId();
+            contact.chatId = chatId
+            contact.status = status || 1
+            contact.save(function (err) {
+                if (err) { return console.log('error', err) }
+                console.log('New chatId!')
+            });
+        } else {
+            console.log('Exist!');
+        }
     });
 
-    let contact = new ChatId();
-    contact.chatId = chatId
-    contact.status = status || 1
-// save the contact and check for errors
-    contact.save(function (err) {
-        if (err) { return console.log('error', err) }
-        console.log('New chatId')
-    });
 };
 
 exports.get = function (chatId) {
     ChatId.get(function (err, chatIds) {
         if (err) { return console.log('error', err); }
-        // console.log(chatIds);
-    });
-    ChatId.findOne({ chatId: chatId }, function (err, adventure) {
-        if (err) { return console.log("err", err) }
-        console.log(adventure);
+        console.log(chatIds);
     });
 };
